027ab6f3845ae7e7a0f5e080629d730966cf0820
Salli 95 - Vivobook <ravileviskander@gmail.com>
2024-03-21 23:46:14 +0500

    добавили сохранение в файл

--- парсингн мани.py
+++ парсингн мани.py
@@ -1,9 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+
+def save_text(text, file_path):
+  with open(file_path, "w") as file:
+      file.write(text)
 
 url = 'https://www.x-rates.com/table/?from=USD&amount=1'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html')
 quotes = soup.find_all('table', class_='tablesorter ratesTable')
+
+file_path = "quotes.txt"
+
 for quote in quotes:
+  save_text(quote.text,file_path)
   print(quote.text)
