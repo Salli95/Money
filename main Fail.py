c411d2661b08fa7e7d19807653ad6821093eaaca
Salli 95 - Vivobook <ravileviskander@gmail.com>
2024-03-21 20:14:57 +0500

    Накидали основной интерфейс

--- main Fail.py
+++ main Fail.py
@@ -1,7 +1,26 @@
 from tkinter import *
+from tkinter.ttk import Combobox  
+
+def clicker():
+    d
 
 window =Tk()
 window.title("Пересчет доларов")
-hello = Label(window, text="Hello")
+window.geometry('500x400')
+
+hello = Label(window, text="С какой валюты будем переводить ?" ,font = ("Arial bold",20))
 hello.grid(column=0 ,row=0)
+combo = Combobox(window)
+combo['values'] = ("Доллар","Тенге","Евро") 
+combo.grid(column=0, row=1) 
+
+hello = Label(window, text="В какую валюту будем переволить? ?" ,font = ("Arial bold",20))
+hello.grid(column=0 ,row=4)
+secondVAL= Combobox(window)
+secondVAL['values'] = ("Доллар","Тенге","Евро") 
+secondVAL.grid(column=0, row=5) 
+
+Button=Button(window,text="Посчитать ", command=clicker)
+Button.grid(column=0 , row=6)
+
 window.mainloop()
