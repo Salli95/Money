6c483036c1638d81de0e09de82d1dbb5cd3fc41b
Salli 95 - Vivobook <ravileviskander@gmail.com>
2024-04-03 20:19:35 +0500

    сделали вывод результата

--- main Fail.py
+++ main Fail.py
@@ -10,29 +10,27 @@ def clicker():
     
     if comboFERST == "Доллар" and comboSECOND=="Тенге":
      Dollar=int(sam)/float(Tenge)
-       
+     label.config(text=Dollar)
      print(Dollar)
+    else:
+        print("Ошибка")
+        label.config(text="Ошибка")
      
-        
-        
-        
-        
     # print("первый:",comboFERST)
     # print("второй:",comboSECOND)
     # print("сумма :",sam)
-    
-
+        
 window =Tk()
 window.title("Пересчет доларов")
 window.geometry('500x400')
 
-hello = Label(window, text="С какой валюты будем переводить ?" ,font = ("Arial bold",20))
+hello = Label(window, text="С какой валюты будем переводить ?" ,font = ("Arial bold",18))
 hello.grid(column=0 ,row=0)
 firstVAL = Combobox(window)
 firstVAL['values'] = ("Доллар","Тенге","Евро","Рубль") 
 firstVAL.grid(column=0, row=1) 
 
-hello = Label(window, text="В какую валюту будем переволить ?" ,font = ("Arial bold",20))
+hello = Label(window, text="В какую валюту будем переволить ?" ,font = ("Arial bold",18))
 hello.grid(column=0 ,row=4)
 secondVAL= Combobox(window)
 secondVAL['values'] = ("Доллар","Тенге","Евро","Рубль") 
@@ -47,5 +45,8 @@ txt.grid(column=0,row=7)
 
 Button=Button(window,text="Посчитать ", command=clicker)
 Button.grid(column=0 , row=8)
+
+label = Label(text="//////////")
+label.grid(column=0 , row=9)
 
 window.mainloop()
